d02a0967b90588d4c9b2e7b586a3fe3d2fdd8f25
Rites17 <64261620+Rites17@users.noreply.github.com>
2020-05-13 21:54:21 +0530

    Add files via upload

---
+++ Handson 7/Solution.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+   
+    static String timeConversion(String s) {
+        
+        int hour = Integer.parseInt(s.substring(0, 2)) % 12;
+    if (s.endsWith("PM"))
+        hour += 12;
+    return String.format("%02d", hour) + s.substring(2, 8);
+
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scan.nextLine();
+
+        String result = timeConversion(s);
+
+        bw.write(result);
+        bw.newLine();
+
+        bw.close();
+    }
+}
